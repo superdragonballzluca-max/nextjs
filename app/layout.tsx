8314c5a7d3d0ba56c7c4705c1d38072dba29fc8e
LucaEpitech <72383563+LucaEpitech@users.noreply.github.com>
2026-06-29 10:23:53 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
   return (
     <html
       lang="en">
-      <body className="{raleway.className}>{children}</body>
+      <body className={raleway.className}>{children}</body>
     </html>
   );
 }
