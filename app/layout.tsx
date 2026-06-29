6b285e65c669728eb38becca89a65921f7ab3d3e
LucaEpitech <72383563+LucaEpitech@users.noreply.github.com>
2026-06-29 10:21:41 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,11 @@
 
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Raleway } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+const raleway  = Raleway({ subsets: ["latin"]
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Nightforge Performance",
@@ -25,10 +19,8 @@ export default function RootLayout({
 }>) {
   return (
     <html
-      lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
-    >
-      <body className="min-h-full flex flex-col">{children}</body>
+      lang="en">
+      <body className="{raleway.className}>{children}</body>
     </html>
   );
 }
