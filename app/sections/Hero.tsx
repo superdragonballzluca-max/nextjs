2f97bcd829e488764a56921197a0a68a1a3c86e6
LucaEpitech <72383563+LucaEpitech@users.noreply.github.com>
2026-06-30 16:33:35 +0200

    update

--- app/sections/Hero.tsx
+++ app/sections/Hero.tsx
@@ -25,6 +25,27 @@ export default function Hero() {
                         loop
                     />
                 </p>
+                <div className="social-links">
+    <a href="#" className="twitter">
+        <i className="bx bxl-twitter"></i>
+    </a>
+
+    <a href="#" className="facebook">
+        <i className="bx bxl-facebook"></i>
+    </a>
+
+    <a href="#" className="instagram">
+        <i className="bx bxl-instagram"></i>
+    </a>
+
+    <a href="#" className="google-plus">
+        <i className="bx bxl-skype"></i>
+    </a>
+
+    <a href="#" className="linkedin">
+        <i className="bx bxl-linkedin"></i>
+    </a>
+</div>
             </div>
         </section>
     );
