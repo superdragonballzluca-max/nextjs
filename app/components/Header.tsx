144e236b51c5658949d1753f6cffffb14c8a311e
LucaEpitech <72383563+LucaEpitech@users.noreply.github.com>
2026-06-30 09:51:13 +0200

    test

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import './header.css';
+import Nav from './Nav';
 
 export default function Header() {
   return (
-    <header id="header" className="d-flex flex-column justify-content-center">
+    <Header id="header" className="d-flex flex-column justify-content-center">
       <Nav>
-        
+
       </Nav>
-    </header>
+    </Header>
   );
 }
