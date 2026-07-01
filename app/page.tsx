dde79fa1e9c44a84ff644302808108a05b4b7db4
LucaEpitech <72383563+LucaEpitech@users.noreply.github.com>
2026-07-01 14:58:59 +0200

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Hero from './sections/Hero';
 import About from './sections/About';
 import Facts from './sections/Facts';
+import Resume from './sections/Resume'
 
 export default function Home() {
   return (
@@ -9,6 +10,7 @@ export default function Home() {
       <main id="id">
         <About />
         <Facts />
+        <Resume/>
       </main>
     </>
   );
--- app/sections/Resume.tsx
+++ app/sections/Resume.tsx
@@ -1,8 +1,45 @@
 import React from 'react';
 import './resume.css';
+import SectionTitle from '../components/SectionTitle';
 
-export default function Resume() {
+async function getResumeData() {
+  const res = await fetch('http://localhost:3001/api/resume');
+  return res.json();
+}
+
+export default async function Resume() {
+  const items : [] | any = await getResumeData();
+
   return (
-    <div>Resume</div>
-  )
+    <section id="resume" className="resume">
+      <div className="container">
+        <SectionTitle
+          title="Resume"
+          brief="Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga
+           eum quidem. Sit sint consectetur velit. Quisquam quos quisquam cupiditate. Et nemo qui i
+           mpedit suscipit alias ea. Quia fugiat sit in iste officiis commodi quidem hic quas."
+        />
+
+        <div className="row">
+  <div className="col-lg-6">
+    <h3 className="resume-title">{items[0].category}</h3>
+
+    {items &&
+      items.length > 0 &&
+      items[0].content.map(
+        (item: {
+          id: number;
+          title: string;
+          period: string;
+          brief: string;
+          details: [string];
+        }) => <ResumeItem key={item.id} item={item} />
+      )}
+  </div>
+
+  <div className="col-lg-6"></div>
+</div>
+      </div>
+    </section>
+  );
 }
