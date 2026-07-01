c54ac9e224d3769730dced782cfb7382d5502fda
LucaEpitech <72383563+LucaEpitech@users.noreply.github.com>
2026-07-01 09:46:29 +0200

    test

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,14 @@
 import Hero from './sections/Hero';
 import About from './sections/About';
+import Facts from './sections/Facts';
 
 export default function Home() {
   return (
     <>
       <Hero />
-
-      <main id="main">
+      <main id="id">
         <About />
+        <Facts />
       </main>
     </>
   );
--- app/sections/Facts.tsx
+++ app/sections/Facts.tsx
@@ -1,7 +1,18 @@
-import React from 'react'
+'use client';
+
+import React, { useState, useEffect } from 'react';
+import { counterData } from '..data/data';
+import SectionTitle from '../components/SectionTitle';
 
 export default function Facts() {
   return (
-    <div>Facts</div>
-  )
+    <section id="facts" className="facts">
+      <div className="container">
+        <SectionTitle
+          title="Facts"
+          brief="Magnam dolores commodi suscipit..."
+        />
+      </div>
+    </section>
+  );
 }
